ceb942321c4b0884bee3d2c22b49f86a13193b21
Minitialize <53800245+Minitialize@users.noreply.github.com>
2021-03-06 23:23:06 +0800

    Update my.js

--- js/my.js
+++ js/my.js
@@ -1,1 +1,38 @@
-console.log();
+const data = [{
+"id": "1",
+"mon": "January"
+},{
+"id": "2",
+"mon": "February"
+},{
+"id":"3",
+"mon": "March"
+},{
+"id":"4",
+"mon": "April"
+},{
+"id":"5",
+"mon":"May"
+},{
+"id":"6",
+"mon":"June"
+},{
+"id":"7",
+"mon":"July"
+},{
+"id":"8",
+"mon":"August"
+},{
+"id":"9",
+"mon":"September"
+},{
+ "id":"10",
+ "mon":"October"   
+},{
+"id":"11",
+"mon":"November"
+},{
+"id":"12",
+"mon":"December"
+}]
+
